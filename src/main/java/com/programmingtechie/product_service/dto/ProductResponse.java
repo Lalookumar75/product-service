4ab93ac2fd1266dd5e359018949c9b805a4c5d9f
Lalookumar19227 <imykl27@gmail.com>
2025-03-03 19:10:20 +0530

    Initial commit

---
+++ src/main/java/com/programmingtechie/product_service/dto/ProductResponse.java
@@ -0,0 +1,66 @@
+package com.programmingtechie.product_service.dto;
+
+import java.math.BigDecimal;
+
+public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    // Default Constructor
+    public ProductResponse() {}
+
+    // Parameterized Constructor
+    public ProductResponse(String id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    // toString() method
+    @Override
+    public String toString() {
+        return "ProductResponse{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
